612a339629a89114ea9564931913e5b2fc601c90
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2025-08-27 11:29:33 +0200

    feat: update exercises hello_world

---
+++ Hola_mundo/ejercicios.py
@@ -0,0 +1,50 @@
+#Ejercicio 1
+print("Hola mundo")
+
+#Ejercicio 2
+# El código del ejercicio 1 imprime "Hola mundo" por consola
+
+#Ejercicio 3
+print("Ismael", 33)
+
+#Ejercicio 4
+print(type("Hola"))
+print(type(24))
+print(type(9.5))
+
+#Ejercicio 5
+"""
+Los tipos de datos en Python son:
+-str: para cadenas de texto
+-int: para números enteros
+-float: para números decimalos o no enteros
+-complex: para datos complejos
+-bool: para valores booleanos
+"""
+
+#Ejercicio 6
+print("Hola" + " " + "mundo")
+
+#Ejercicio 7
+Nombre= "Ismael"
+Edad= 33
+print(Nombre, Edad)
+
+#Ejercicio 8
+nombre=input("Introduzca su nombre: ")
+print("Hola, bienvenido", nombre)
+
+#Ejercicio 9
+numero1=6
+numero2=5
+suma= numero1+numero2
+print("Si sumamos", numero1 ,"más", numero2 ,"el resultado es", suma, ". El tipo de dato del resultado es", type(suma))
+
+#Ejercicio 10
+# Definir dos números enteros
+numero1=6
+numero2=5
+# Suma de los números enteros
+suma= numero1+numero2
+# En el mismo Print imprime la suma de los dos números enteros y luego define el tipo de dato
+print("Si sumamos", numero1 ,"más", numero2 ,"el resultado es", suma, ". El tipo de dato del resultado es", type(suma))
