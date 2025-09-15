d90d7c5e89487449017b9957fbc8b1e784588926
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2025-09-15 16:51:11 +0200

    feat: update variables

---
+++ Python_desde_cero/2.Variables/variables.py
@@ -0,0 +1,47 @@
+# Variables
+
+my_string_variable = "My String variable"
+print(my_string_variable)
+
+my_int_variable = 5
+print(my_int_variable)
+
+my_int_to_str_variable = str(my_int_variable)
+print(my_int_to_str_variable)
+print(type(my_int_to_str_variable))
+
+my_bool_variable = False
+print(my_bool_variable)
+
+# Concatenación de variables en un print
+print(my_string_variable, my_int_to_str_variable, my_bool_variable)
+print("Este es el valor de:", my_bool_variable)
+
+# Algunas funciones del sistema
+print(len(my_string_variable))
+
+# Variables en una sola línea. ¡Cuidado con abusar de esta sintaxís!
+name, surname, alias, age = "Ismael", "Hernandez", "RiosSev", 33
+print("Me llamo:", name, surname, ". Tengo:", age, "años. Y mi alias es:", alias)
+
+# Inputs
+"""
+name = input('¿Cual es tu nombre?')
+age = input('¿Cuantos años tienes?')
+
+print(name)
+print(age)
+"""
+# Cambiamos su tipo
+name = 33
+age = "Ismael"
+
+print(name)
+print(age)
+
+# Forzamos el tipo
+address: str = "Mi dirección"
+address = 32
+address = 5.2
+address = False
+print(type(address))
