1e14982524e845a739a2a04a1889caa4c6c26664
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2025-09-16 13:19:16 +0200

    feat: update ejercicios variables

---
+++ Python_desde_cero/2.Variables/2_ejercicios.py
@@ -0,0 +1,52 @@
+#Ejercicio 1
+name = "Ismael"
+age = 33
+height = 1.71
+
+print(name)
+print(age)
+print(height)
+
+#Ejercicio 2
+print("Hola, tengo " + str(age) + " años.")
+
+#Ejercicio 3
+is_student = False
+print("¿Soy estudiante?", is_student)
+
+#Ejercicio 4
+nombre_completo = "Ismael Hernandez Rios"
+print(len(nombre_completo))
+
+#Ejercicio 5
+nombre, apellido, ciudad = "Ismael", "Hernandez", "Sevilla"
+print(nombre, apellido, ciudad)
+
+#Ejercicio 6
+color = input("¿Cual es tu color favorito?")
+print(color)
+
+#Ejercicio 7
+fruit = "manzana"
+print(fruit)
+fruit = "sandia"
+print(fruit)
+
+#Ejercicio 8
+price = 2.99
+price_entero = int(price)
+print(price_entero)
+# Tambien valdria asi
+price = 2.99
+print(int(price))
+
+#Ejercicio 9
+address = "Calle Fernando Hierro 3A, Guillena"
+address_len = len(address)
+print(address_len)
+
+#Ejercicio 10
+phone: int = 615053570
+print(type(phone))
+phone = 666111888
+print(type(phone))
