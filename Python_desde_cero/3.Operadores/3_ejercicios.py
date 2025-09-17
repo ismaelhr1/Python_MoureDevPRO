0d775a68b8cf9bd84c2e2038c5828384dec9a159
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2025-09-17 14:20:12 +0200

    feat: update ejercicios operadores

--- Python_desde_cero/3.Operadores/3_ejercicios.py
+++ Python_desde_cero/3.Operadores/3_ejercicios.py
@@ -5,3 +5,40 @@ print(8 * 7)
 print(100 / 20)
 
 # Ejercicio 2
+remainder = 37 % 5
+print(remainder)
+
+# Ejercicio 3
+print(str(7) + " es mi número favorito")
+# tambien sirve así
+number = 7
+result = str(number) + " es mi número favorito"
+print(result)
+
+# Ejercicio 4
+print("Python " * 10)
+
+# Ejercicio 5
+a = 12
+b = 8
+resultado = a > b
+print(resultado)
+
+# Ejercicio 6
+print("apple" > "banana")
+print("apple" < "banana")
+
+# Ejercicio 7
+print(10 > 5 and 10 < 20)
+
+# Ejercicio 8
+print(7 < 3 or 7 > 5)
+
+# Ejercicio 9
+print(not(15 > 20))
+
+# Ejercicio 10
+operacion= 5 * 3 + 2
+print(operacion > 10 and operacion < 20)
+# tambien sirve asi
+print((5 * 3) + 2 > 10 and (5 * 3) + 2 < 20)
