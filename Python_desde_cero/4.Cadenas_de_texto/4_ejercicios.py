fd0a8d8af5833fafaaba6274d5112e7758fc1d31
ismaelhr1 <122208617+ismaelhr1@users.noreply.github.com>
2025-09-18 11:34:07 +0200

    feat: update ejercicios strings

---
+++ Python_desde_cero/4.Cadenas_de_texto/4_ejercicios.py
@@ -0,0 +1,46 @@
+# Ejercicio 1
+text = "Aprendiendo Python"
+print(len(text))
+
+# Ejercicio 2
+print("Hola" + " " + "Python")
+
+# Ejercicio 3
+salto = "Esto es una prueba \ncon salto de línea"
+print(salto)
+
+# Ejercicio 4
+nombre, apellido, edad = "Ismael", "Hernandez", 33
+print(f"Me llamo {nombre} {apellido} y tengo {edad} años")
+
+# Ejercicio 5
+palabra = "Python"
+a, b, c, d, e, f = palabra
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+
+# Ejercicio 6
+palabra = "Programación"
+palabra_slice = palabra[3:8]
+print(palabra_slice)
+
+# Ejercicio 7
+palabra = "Python"
+palabra_reversed = palabra[::-1]
+print(palabra_reversed)
+
+# Ejercicio 8
+text = "aprendiendo Python"
+print(text.upper())
+
+# Ejercicio 9
+text = "Programación en Python"
+print(text.count("n"))
+
+# Ejercicio 10
+text = "12345"
+print(text.isnumeric())
